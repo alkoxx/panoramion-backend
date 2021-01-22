72e327b43859db16a5f164e9ab696099b1d6a543
alkoxx <jonjon.a@gmail.com>
2021-01-23 00:27:44 +0100

    Added security control for testing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
-class SecurityController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods="POST")
      */
     public function login(){
-        
+        return $this->json([
+            'user' => $this->getUser() ? $this->getUser()->getId() : null]
+        );
     }
 
 }
