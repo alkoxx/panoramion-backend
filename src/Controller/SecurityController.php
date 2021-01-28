3211f00957ca01a5d3a15b7865502e2d38f7ea1b
alkoxx <jonjon.a@gmail.com>
2021-01-29 00:08:20 +0100

    Return location header

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login", methods="POST")
      */
-    public function login(){
-        return $this->json([
-            'user' => $this->getUser() ? $this->getUser()->getId() : null]
-        );
+    public function login(IriConverterInterface $iriConverter){
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser())
+        ]);
     }
 
 }
